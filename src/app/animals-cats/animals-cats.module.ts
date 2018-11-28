7221931cf991d639ee53596071f7180f95674b46
Tuan Nguyen Manh <tuannm.cntt93@gmail.com>
2018-11-29 01:54:29 +0700

    add module animals-cats

---
+++ src/app/animals-cats/animals-cats.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AnimalsCatsRoutingModule } from './animals-cats-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AnimalsCatsRoutingModule
+  ],
+  declarations: []
+})
+export class AnimalsCatsModule { }
